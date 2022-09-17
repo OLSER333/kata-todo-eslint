4bd97e48e5c7f008c572ab40ab4561004cc5069c
olser <gritsienko.oleg@mail.ru>
2022-09-17 15:45:07 +0300

    corrections

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,23 +4,25 @@ import PropTypes from 'prop-types'
 import Task from './Task'
 
 const TaskList = (props) => {
-  const items = props.dataList.map((el) => {
-    const { id, done, ...otherData } = el
+  return (
+    <ul className="todo-list">
+      {props.dataList.map((el) => {
+        const { id, done, ...otherData } = el
 
-    return (
-      <li className={done ? 'completed' : null} key={id}>
-        <Task
-          // onActive={() => props.onActive(id)}
-          onCompleted={() => props.onCompleted(id)}
-          onDeleted={() => props.onDeleted(id)}
-          done={done}
-          {...otherData}
-        ></Task>
-      </li>
-    )
-  })
-
-  return <ul className="todo-list">{items}</ul>
+        return (
+          <li className={done ? 'completed' : null} key={id}>
+            <Task
+              // onActive={() => props.onActive(id)}
+              onCompleted={() => props.onCompleted(id)}
+              onDeleted={() => props.onDeleted(id)}
+              done={done}
+              {...otherData}
+            ></Task>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 TaskList.defaultProps = {
   id: Math.round(Math.random() * Date.now()),
